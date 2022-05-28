f8cc5a3fcfc5e6f9a91e4f9c0a2d2ab5e9b22aaf
Joona <joonatan.karhu@outlook.com>
2022-05-29 00:49:15 +0300

    added tailwind

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className="py-4 w-full flex bg-green">
+        <div>One</div>
+        <div>Two</div>
+        <div>Three</div>
+    </div>
+  )
+}
+
+export default Navbar
