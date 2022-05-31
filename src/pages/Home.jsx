0b2a311959b081ebc9ea3302484ec2ca7e305a31
Joona <joonatan.karhu@outlook.com>
2022-05-31 18:41:39 +0300

    fixed image alt description

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,7 +5,7 @@ const Home = () => {
   return (
     <div>
       <div className='bg-gradient-to-tr from-indigo-900 to-indigo-800 h-96 w-full md:top-0 relative'>
-        <img src={bannerImage} className="w-full h-full object-cover absolute mix-blend-overlay" />
+        <img src={bannerImage} className="w-full h-full object-cover absolute mix-blend-overlay" alt="Landing Page image" />
         <div className='p-20 px-10 md:px-20 md:py-20 lg:py-30'>
           <h1 className='text-white text-5xl md:text-7xl lg:text-8xl mb-2 font-bold'>Upgrade your<br/>wardrobe today!</h1>
           <p className='text-white text-3xl font-light mb-10'>The Latest Fashion trends has arrived.</p>
