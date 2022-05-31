b37a807af1800d1470e1b087afe6b9d1f3e1844a
Joona <joonatan.karhu@outlook.com>
2022-05-31 20:14:31 +0300

    Changed Favicon Color

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,7 +8,7 @@ const Home = () => {
         <img src={bannerImage} className="w-full h-full object-cover absolute mix-blend-overlay" alt="Landing Page image" />
         <div className='p-20 px-10 md:px-20 md:py-20 lg:py-30'>
           <h1 className='text-white text-5xl md:text-7xl lg:text-8xl mb-2 font-bold'>Upgrade your<br/>wardrobe today!</h1>
-          <p className='text-white text-3xl font-light mb-10'>The Latest Fashion trends has arrived.</p>
+          <p className='text-white text-3xl font-light mb-10'>The Latest Fashion trends have arrived.</p>
         </div>
       </div>
     </div>
