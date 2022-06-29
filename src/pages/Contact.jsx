943c733b8b15b5800c9877cae699fbdae3e11c41
Joona <joonatan.karhu@outlook.com>
2022-06-30 01:57:29 +0300

    f

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -6,10 +6,12 @@ const Contact = () => {
     <div className='bg-indigo-200 p-12 md:p-15 flex items-center justify-center'>
       <div className='bg-white w-full md:max-w-xs p-10 rounded-lg'>
         <h1 className='text-3xl text-center mb-10'>Contact Us</h1>
-        <form name="customerContact" action="POST" data-netlify="true" className='flex flex-col'>
-          
-          <input required className='border p-2 rounded py-1 mb-4 placeholder:italic placeholder:text-slate-400' type="text" id="customerEmail" name="email" aria-aria-label='Email' placeholder='Email'/>
-          <textarea required className='border p-2 rounded py-1 mb-5 resize-none placeholder:italic placeholder:text-slate-400' type="textarea" name="customerMessage" placeholder='Message...' />
+        <form name="contact" action="POST" data-netlify="true" className='flex flex-col'>
+          <input type="hidden" name="contact" value="contact" />
+        
+          <input required className='border p-2 rounded py-1 mb-4 placeholder:italic placeholder:text-slate-400' type="email" id="customerEmail" name="email" aria-aria-label='Email' placeholder='Email' />
+
+          <textarea required className='border p-2 rounded py-1 mb-5 resize-none placeholder:italic placeholder:text-slate-400' type="textarea" name="message" placeholder='Message...' />
           <Button type="submit">Send</Button>
         </form>
 
